9fbde64edc7986dfce3ee7113e1f0faa29b9097d
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2025-05-27 08:52:07 +0700

    Create One-Liner for print update file.sh

---
+++ Scripting/Bash/One-Liner for print update file.sh
@@ -0,0 +1,1 @@
+f="a.txt"; p="pos.json"; [ -f "$p" ] || echo '{}' > "$p"; last=$(jq -r --arg f "$f" '.[$f]//0' "$p"); current=$(wc -l < "$f" | tr -d ' '); diff=$((current - last)); [ "$diff" -gt 0 ] && tail -n "$diff" "$f"; jq --arg f "$f" --argjson pos "$current" '.[$f]=$pos' "$p" > tmp.$$.json && mv tmp.$$.json "$p"
